e6e766906b40a6ccf4f203d898797e80328ac73a
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2024-12-28 21:50:27 +0000

    Add basic map

---
+++ src/lib/map-state.svelte.ts
@@ -0,0 +1,2 @@
+export const center = $state<[number, number]>([-122.34062, 47.60556]);
+export const zoom = $state(14);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
 	import '../app.css';
+
+	// Components
+	import { MapLibre } from 'svelte-maplibre';
+
+	// State
+	import { center, zoom } from '$lib/map-state.svelte';
 </script>
 
-<h1>Monad Map</h1>
+<div class="grid h-svh grid-cols-12">
+	<MapLibre
+		style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+		standardControls
+		class="col-span-6 h-full"
+		{zoom}
+		{center}
+	></MapLibre>
+	<div>Hello</div>
+</div>
