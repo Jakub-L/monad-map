5b596d6d72c3273c972f6974437ca4eb3b2c5620
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-01 21:47:29 +0000

    Convert PoI to use bits-ui collapsible

--- src/lib/components/point-of-interest.svelte
+++ src/lib/components/point-of-interest.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import type { PoI } from '$lib/types';
-	import Input from './input.svelte';
+	import { Collapsible } from 'bits-ui';
 	import IconDown from '~icons/ion/caret-down-sharp';
-
+	import { slide } from 'svelte/transition';
+	
 	interface Props {
 		marker: PoI;
 		index: number;
@@ -14,34 +15,33 @@
 	const toggle = () => (isExpanded = !isExpanded);
 </script>
 
-<div id="container" class="flex w-full flex-col border border-red-500 px-2 py-1">
-	<div class="flex items-center gap-2">
-		<span class="min-w-6 text-center font-nova text-xl">{index + 1}</span>
-		<h3 class="grow truncate font-nova">{marker.title}</h3>
-		<button
-			onclick={toggle}
-			class="group group flex min-h-8 min-w-8 items-center justify-center self-start rounded-full p-0.5 hover:bg-red-100/15 hover:text-red-500 active:bg-red-100/15 active:text-red-800"
-		>
-			<IconDown
-				class={[
-					'h-5 w-5 transition-all duration-200 group-active:scale-90',
-					isExpanded && '-scale-100'
-				]}
-			/>
-		</button>
-	</div>
-	<p
-		class={[
-			'ml-8 mr-10 overflow-hidden border-t border-red-500/50 pt-1 text-sm opacity-85',
-			!isExpanded && 'truncate'
-		]}
+<Collapsible.Root id="container" class="flex w-full items-center border border-red-500 p-2">
+	<Collapsible.Trigger
+		onclick={toggle}
+		class="group group flex min-h-8 min-w-8 items-center justify-center self-start rounded-full p-0.5 hover:bg-red-100/15 hover:text-red-500 active:bg-red-100/15 active:text-red-800"
 	>
-		{marker.description}
-	</p>
-</div>
+		<IconDown
+			class={[
+				'h-5 w-5 transition-all duration-200 group-active:scale-90',
+				isExpanded && '-scale-100'
+			]}
+		/>
+	</Collapsible.Trigger>
+	<div class="flex flex-col">
+		<div class="flex items-center gap-2 pr-4">
+			<span class="min-w-6 text-center font-nova text-xl">{index + 1}</span>
+			<h3 class="grow truncate font-nova">{marker.title}</h3>
+		</div>
+		<Collapsible.Content transition={slide}>
+			<p class="mx-8 overflow-hidden border-t border-red-500/50 pt-1 text-sm opacity-85">
+				{marker.description}
+			</p>
+		</Collapsible.Content>
+	</div>
+</Collapsible.Root>
 
 <style>
-	#container {
+	:global(#container) {
 		clip-path: polygon(0 0, 100% 0, 100% calc(100% - 2rem), calc(100% - 2rem) 100%, 0 100%);
 		background: linear-gradient(
 				135deg,
