b4f3b9488f6e29a36f844d948464070f7d374c2b
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-03 21:24:44 +0000

    Style toolbar

--- src/lib/components/toolbar.svelte
+++ src/lib/components/toolbar.svelte
@@ -1,20 +1,38 @@
 <script lang="ts">
 	import { selectedMapId, maps } from '$lib/state.svelte';
+	import LoadMapDialog from './load-map-dialog.svelte';
 
 	const selectedId = selectedMapId.value;
 	const selectedMap = maps.value[selectedId];
+	let loadDialogOpen = false;
 
-	const clearMarkers = () => {
-		selectedMap.markers = [];
-	};
+	const clearMarkers = () => (selectedMap.markers = []);
+	const toggleLoadDialog = () => (loadDialogOpen = !loadDialogOpen);
 </script>
 
-<div class="flex gap-10">
+{#snippet toolbarButton(text: string, fn?: () => void)}
+	<button
+		class="min-w-20 rounded-full border border-red-500 px-2 py-1 hover:bg-red-700/30 focus-visible:outline focus-visible:outline-4 focus-visible:-outline-offset-1 focus-visible:outline-red-400 active:scale-95 active:bg-red-700/60"
+		onclick={fn}>{text}</button
+	>
+{/snippet}
+
+<div class="flex items-center gap-6">
 	<h1 class="m-2 font-nova text-3xl uppercase">Monad//Map</h1>
-	<div class="flex gap-2">
-		<button>New</button>
-		<button>Save As</button>
-		<button>Export</button>
-		<button onclick={clearMarkers}>Clear Markers</button>
+	<div class="h-8 border-l border-red-500/50"></div>
+	<span class="font-nova text-xl">{selectedMap.name}</span>
+	<div class="flex items-center gap-2">
+		{@render toolbarButton('New')}
+		{@render toolbarButton('Load', toggleLoadDialog)}
+		{@render toolbarButton('Rename')}
+		{@render toolbarButton('Clone')}
+		<div class="h-6 border-l border-red-500/50"></div>
+		{@render toolbarButton('Export')}
+		{@render toolbarButton('Import')}
+		{@render toolbarButton('Print')}
+		<div class="h-6 border-l border-red-500/50"></div>
+		{@render toolbarButton('Clear', clearMarkers)}
+		{@render toolbarButton('Delete')}
 	</div>
 </div>
+<LoadMapDialog bind:open={loadDialogOpen} />
