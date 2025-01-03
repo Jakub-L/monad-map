109195135b05edf845eba245d5bec52d7a2fe76b
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-03 21:44:39 +0000

    Support creating a new, blank map

--- src/lib/components/toolbar.svelte
+++ src/lib/components/toolbar.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
-	import { selectedMapId, maps } from '$lib/state.svelte';
 	import { nanoid } from 'nanoid';
-	import LoadMapDialog from './load-map-dialog.svelte';
+	import { DEFAULT_CENTER, DEFAULT_ZOOM } from '$lib/constants';
+	import { selectedMapId, maps } from '$lib/state.svelte';
+	import LoadMapDialog from '$lib/components/load-map-dialog.svelte';
+	import type { MonadMap } from '$lib/types';
 
 	// STATE
 	const selectedId = $derived(selectedMapId.value);
 	const selectedMap = $derived(maps.value[selectedId]);
 	let loadDialogOpen = $state(false);
 
+	// UTILS
+	const saveAndActivate = (map: MonadMap) => {
+		maps.value = { ...maps.value, [map.id]: map };
+		selectedMapId.value = map.id;
+	};
+
 	// HANDLERS
 	const toggleLoadDialog = () => (loadDialogOpen = !loadDialogOpen);
 
+	const createNewMap = () => {
+		saveAndActivate({
+			id: nanoid(),
+			name: 'Unnamed map',
+			center: DEFAULT_CENTER,
+			zoom: DEFAULT_ZOOM,
+			markers: []
+		});
+	};
+
 	/** Clones current map and activates it */
 	const cloneMap = () => {
-		const newMap = { ...selectedMap, name: `${selectedMap.name} (copy)`, id: nanoid() };
-		maps.value = { ...maps.value, [newMap.id]: newMap };
-		selectedMapId.value = newMap.id;
+		saveAndActivate({ ...selectedMap, name: `${selectedMap.name} (copy)`, id: nanoid() });
 	};
 
 	/** Clears all markers from the map */
@@ -32,9 +48,9 @@
 <div class="flex items-center gap-6">
 	<h1 class="m-2 font-nova text-3xl uppercase">Monad//Map</h1>
 	<div class="h-8 border-l border-red-500/50"></div>
-	<span class="font-nova text-xl">{selectedMap.name}</span>
+	<span class="font-nova text-xl">{selectedMap.name ?? 'No map loaded'}</span>
 	<div class="flex items-center gap-2">
-		{@render toolbarButton('New')}
+		{@render toolbarButton('New', createNewMap)}
 		{@render toolbarButton('Load', toggleLoadDialog)}
 		{@render toolbarButton('Rename')}
 		{@render toolbarButton('Clone', cloneMap)}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,5 @@
 export const MAX_MARKERS: number = 50;
+
+// MAP SPECIFIC
+export const DEFAULT_CENTER: [number, number] = [-122.34062, 47.60556];
+export const DEFAULT_ZOOM: number = 14;
