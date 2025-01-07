09199979283abeb737fee6382c9a506fa7559587
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-01-07 22:22:58 +0000

    Improve behaviour of textarea

--- src/lib/components/point-of-interest.svelte
+++ src/lib/components/point-of-interest.svelte
@@ -2,6 +2,8 @@
 	import type { Component } from 'svelte';
 	import type { PoI } from '$lib/types';
 
+	import { MAX_DESCRIPTION, MAX_TITLE } from '$lib/constants';
+
 	// Icons
 	import IconCheck from '~icons/ion/checkmark-sharp';
 	import IconClose from '~icons/ion/close-sharp';
@@ -22,6 +24,7 @@
 	let isEditing: boolean = $state(false);
 	let editTitle: string = $state('');
 	let editDescription: string = $state('');
+	let textArea: HTMLTextAreaElement;
 
 	// Handlers
 	const onEdit = () => {
@@ -46,7 +49,8 @@
 	};
 
 	const onKeyDown = (event: KeyboardEvent): void => {
-		if (event.key === 'Enter' || event.key === 'Return') onSave();
+		const modifierKey = event.shiftKey || event.altKey || event.ctrlKey || event.metaKey;
+		if ((event.key === 'Enter' || event.key === 'Return') && !modifierKey) onSave();
 	};
 </script>
 
@@ -67,12 +71,15 @@
 		{#if isEditing}
 			<input
 				class="h-9 grow rounded-r-full border border-red-500 bg-red-950/40 p-1 font-nova hover:bg-red-700/30 focus:outline focus:outline-4 focus:-outline-offset-1 focus:outline-red-400 focus-visible:bg-red-950/80"
-				bind:value={editTitle}
+				name="title"
 				onkeydown={onKeyDown}
+				maxlength={MAX_TITLE}
+				placeholder="Enter location title..."
+				bind:value={editTitle}
 			/>
 		{:else}
 			<div class="flex h-9 grow items-center px-[5px]">
-				<h3 class="font-nova">{marker.title}</h3>
+				<h3 class="font-nova">{marker.title || 'No point of interest title'}</h3>
 			</div>
 		{/if}
 		<div class="mr-2 flex gap-1">
@@ -86,14 +93,31 @@
 		</div>
 	</div>
 	{#if isEditing}
-		<textarea
-			class="rounded-r-[1.25rem] border border-red-500 bg-red-950/40 p-1 text-left text-sm hover:bg-red-700/30 focus:outline focus:outline-4 focus:-outline-offset-1 focus:outline-red-400 focus-visible:bg-red-950/80"
-			rows={4}
-			bind:value={editDescription}
-		></textarea>
+		<div class="mr-2 flex flex-col">
+			<span class="flex justify-end pr-3 text-xs lowercase">
+				{editDescription.length}/{MAX_DESCRIPTION}
+			</span>
+			<textarea
+				placeholder="Enter location description..."
+				name="description"
+				class="mb-2 rounded-r-[1.25rem] border border-red-500 bg-red-950/40 p-1 text-left text-sm placeholder:text-red-50/90 hover:bg-red-700/30 focus:outline focus:outline-4 focus:-outline-offset-1 focus:outline-red-400 focus-visible:bg-red-950/80"
+				rows={4}
+				maxlength={MAX_DESCRIPTION}
+				bind:value={editDescription}
+				onkeydown={onKeyDown}
+			></textarea>
+		</div>
 	{:else}
-		<p class="border-t border-red-500/50 pt-1 text-sm opacity-85">
-			{marker.description}
+		<p
+			class="mb-2 mr-2 whitespace-pre-wrap border-t border-red-500/50 px-1 pt-1 text-sm opacity-85"
+		>
+			{marker.description || 'No point of interest description.'}
 		</p>
 	{/if}
 </div>
+
+<style>
+	textarea {
+		field-sizing: content;
+	}
+</style>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,7 @@ export const MAX_MARKERS: number = 50;
 // MAP SPECIFIC
 export const DEFAULT_CENTER: [number, number] = [-122.34062, 47.60556];
 export const DEFAULT_ZOOM: number = 14;
+
+// POI SPECIFIC
+export const MAX_TITLE: number = 100;
+export const MAX_DESCRIPTION: number = 500;
